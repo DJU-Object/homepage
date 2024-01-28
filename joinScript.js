aca1d8ae3e361f71d0e2a86f1afff2ffb6253675
jucheolkang <jucheol9877@naver.com>
2024-01-29 02:39:25 +0900

    fix: 지원 버튼 수정

--- joinScript.js
+++ joinScript.js
@@ -1,14 +1,4 @@
-document.addEventListener('DOMContentLoaded', function () {
-    // 버튼 요소들을 선택합니다.
-    const characterButton = document.querySelector('.content:nth-child(1) button');
-    const objectButton = document.querySelector('.content:nth-child(2) button');
-
-    // 각 버튼에 클릭 이벤트 리스너를 추가합니다.
-    characterButton.addEventListener('click', function () {
-        window.location.href = 'https://dojang.io/mod/page/view.php?id=61';
-    });
-
-    objectButton.addEventListener('click', function () {
-        window.location.href = 'https://product.kyobobook.co.kr/detail/S000001766367';
-    });
+document.getElementById('joinus').addEventListener('click', function () {
+    var joinUsPage = document.querySelector('.join-us-page');
+    joinUsPage.style.display = (joinUsPage.style.display === 'none' || joinUsPage.style.display === '') ? 'block' : 'none';
 });
